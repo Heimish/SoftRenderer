645f9ddc4261b2387df316b33e9157ffc23939f8
JJW <light_con@naver.com>
2018-11-12 16:51:12 +0900

    123123123

---
+++ Renderer.cpp
@@ -0,0 +1,90 @@
+
+#include "stdafx.h"
+#include "SoftRenderer.h"
+#include "GDIHelper.h"
+#include "Renderer.h"
+
+#include "Vector.h"
+#include "IntPoint.h"
+
+bool IsInRange(int x, int y);
+void PutPixel(int x, int y);
+
+bool IsInRange(int x, int y)
+{
+	return (abs(x) < (g_nClientWidth / 2)) && (abs(y) < (g_nClientHeight / 2));
+}
+
+void PutPixel(const IntPoint& InPt)
+{
+	PutPixel(InPt.X, InPt.Y);
+}
+
+void PutPixel(int x, int y)
+{
+	if (!IsInRange(x, y)) return;
+
+	ULONG* dest = (ULONG*)g_pBits;
+	DWORD offset = g_nClientWidth * g_nClientHeight / 2 + g_nClientWidth / 2 + x + g_nClientWidth * -y;
+	*(dest + offset) = g_CurrentColor;
+}
+
+
+void UpdateFrame(void)
+{
+	// Buffer Clear
+	SetColor(32, 128, 255);
+	Clear();
+
+	// Draw
+
+	static Vector3 cor;
+	cor.X += 0.7f;
+	cor.Y += 0.5f;
+	cor.Z += 0.9f;
+
+	cor.X = fmodf(cor.X, 255.0f);
+	cor.Y = fmodf(cor.Y, 125.0f);
+	cor.Z = fmodf(cor.Z, 185.0f);
+
+	SetColor(cor.X, cor.Y, cor.Z);
+
+	// Draw a filled circle with radius 100
+	Vector3 center(0.0f, 0.0f);
+	float radius = 100.0f;
+	int nradius = (int)radius;
+
+	static float degree = 0;
+	degree += -1;
+	degree = fmodf(degree, 360.0f);
+
+	Matrix3 rotMat;
+	rotMat.SetRotation(degree);
+	rotMat.Transpose();
+
+	float maxScale = 1;
+	float scale = ((sinf(Deg2Rad(degree * 2)) + 1) * 0.5) * maxScale;
+	if (scale < 0.5f) scale = 0.5f;
+
+	Matrix3 scaleMat;
+	scaleMat.SetScale(scale, scale, scale);
+
+	float maxPos = 250;
+	float pos = sinf(Deg2Rad(degree)) * maxPos;
+	Matrix3 translationMat;
+	translationMat.SetTranslation(-pos, pos);
+
+	Matrix3 SR = scaleMat * rotMat;
+	Matrix3 TRS = translationMat * rotMat * scaleMat;
+
+	for (int i = -nradius; i <= nradius; i++)
+	{
+		for (int j = -nradius; j <= nradius; j++)
+		{
+			PutPixel(Vector3((float)i, (float)j) * TRS);
+		}
+	}
+
+	// Buffer Swap 
+	BufferSwap();
+}
